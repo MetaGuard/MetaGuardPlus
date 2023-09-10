d0362738ef0cd2bcc852c581115e7aa95f3fac59
Vivek Nair <vivek@nair.me>
2023-09-09 18:59:01 -0700

    update miller test

--- evaluation/e03-miller-test.py
+++ evaluation/e03-miller-test.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from tqdm import trange
 
 # Start measuring performance
 start_time = time.time()
@@ -27,18 +28,27 @@ testY = np.repeat(np.arange(N), 300)
 # Train a random forest model
 def trainrf(trainX, trainY):
     rf = RandomForestClassifier(
-        n_estimators=32,
-        n_jobs=32,
+        n_estimators=12,
+        n_jobs=12,
+        max_depth=24,
         verbose=100
     )
     rf.fit(trainX, trainY)
     rf.verbose = 0
     return rf
 
+# Predict in batches
+def batch_predict(rf, testX):
+    BATCH_SIZE = 10000
+    output = []
+    for i in trange(testX.shape[0] // BATCH_SIZE):
+        output.append(rf.predict(testX[BATCH_SIZE*i:BATCH_SIZE*(i+1)]))
+    return np.hstack(output)
+
 # Test a random forest model
 def testrf(rf, testX, name):
     print("--", name, "--")
-    predY = rf.predict(testX)
+    predY = batch_predict(rf, testX)
     acc = accuracy_score(testY, predY)
     print("Accuracy (Per Sample): " + str(acc))
     predY = [np.bincount(x).argmax() for x in predY.reshape(-1, 300)]
