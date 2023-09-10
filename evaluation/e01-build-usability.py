bdfece91fa7795df7e5d7f4b513a2c063162e9a8
Vivek Nair <vivek@nair.me>
2023-09-10 16:56:34 -0700

    add eval replays

--- evaluation/e01-build-usability.py
+++ evaluation/e01-build-usability.py
@@ -31,8 +31,21 @@ pool = {
     ]
 }
 
-# Configuration options
-MIN_REPLAYS = 100             # Minimum number of replays per user for inclusion
+# Define score thresholds
+scores = {
+    'Hard-Standard-51CB5A9D50F73FEAC9380570CEC13A91D3824E64': 438173, # Enemy
+    'Hard-Standard-87EFECA37C255E63F8E1EC28C6ABD94A128631C0': 604650, # Trauma
+    'Hard-Standard-4D03502003602E8E8F0D9F8C84A3E70DA0C389F6': 651382, # 99.9
+    'Hard-Standard-595F76619E3058472A9DE19DAFCEFFD2B5392093': 881963, # Gypsy Tronic
+    'Expert-Standard-43304202EC7681E52B4026313C7AB9099BE2890D': 770665, # 666
+    'Expert-Standard-B25D5435EF97A85549620125C28A28A153CE396F': 859063, # Cheatcode
+    'Expert-Standard-4B6672956A0B64FE7922F5DF932077C716D27808': 483930, # Cambodia
+    'Expert-Standard-816E3A71BB3053B4C1DA3F7A1875C721B2764039': 827993, # Time Leaper
+    'ExpertPlus-Standard-619250FCF1D3A479D53469B9250837391AE449EA': 1142231, # Fresh!
+    'ExpertPlus-Standard-BA220888BB848D07BDFE3C49A4960E487C63BE82': 1068602, # SCREW // owo // SCREW
+    'ExpertPlus-Standard-27860892DD00DF00DA30A3DB12C23A6999EB853F': 2027490, # Night Raid
+    'ExpertPlus-Standard-64688EE557A6C0916D2108AA3B39DD79C8468DB3': 1406657, # eden
+}
 
 # Shuffle map pool
 for cat in ['acc', 'tech', 'speed']:
@@ -59,6 +72,7 @@ groups = ['control', 'artificial', 'metaguard', 'metaguardplus']
 ids = list(range(12))
 np.random.shuffle(ids)
 n = 0
+log = open('./usability/answer-key.txt', 'w')
 for i in range(4):
     group = groups[i]
     for cat in ['acc', 'tech', 'speed']:
@@ -82,6 +96,10 @@ for i in range(4):
 
                 with open('Z:/beatleader/replays/' + replay + '.bsor', 'rb') as f:
                     xror = XROR.fromBSOR(f)
+
+                # Exclude low-scoring replays
+                target_score = scores[map] * 0.96
+                if (xror.data['info']['software']['activity']['score'] < target_score): continue
 
                 frames = handle_bsor('Z:/beatleader/replays/' + replay + '.bsor')
                 times = np.arange(0, 30, 1/30).reshape(900,1)
@@ -125,3 +143,6 @@ for i in range(4):
                 continue
 
         print(id, group, cat, map, c)
+        log.write(",".join([str(id), group, cat, map, str(c)]) + "\n")
+
+log.close()
